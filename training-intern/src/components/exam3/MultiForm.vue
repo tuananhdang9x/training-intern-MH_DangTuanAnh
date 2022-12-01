12b327aec37fe0da165d291b54b8e13fd8fadb50
tuananhdang9x <tuananhdang9x@gmail.com>
2022-12-01 09:08:05 +0700

    Create branch exam3

--- training-intern/src/components/exam3/MultiForm.vue
+++ training-intern/src/components/exam3/MultiForm.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    <h3>Exam 3</h3>
+    <h3 class="header">Đơn ứng tuyển</h3>
+    <div class="nav-bar">
+      <div class="nav-item">
+        <div class="item-number">
+          <p>1</p>
+        </div>
+        <p class="item-info">Thông tin cá nhân</p>
+      </div>
+      <div class="nav-item">
+        <div class="item-number">
+          <p>2</p>
+        </div>
+        <p class="item-info">Kinh nhiệm làm việc</p>
+      </div>
+      <div class="nav-item">
+        <div class="item-number">
+          <p>3</p>
+        </div>
+        <p class="item-info">Xác nhận thông tin</p>
+      </div>
+    </div>
   </div>
 </template>
   
