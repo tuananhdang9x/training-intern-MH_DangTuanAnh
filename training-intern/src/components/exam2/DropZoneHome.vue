5578c05c0f4422e06ce27640fdb96ffc41bdd84f
tuananhdang9x <tuananhdang9x@gmail.com>
2022-12-06 13:40:31 +0700

    reup exam2

--- training-intern/src/components/exam2/DropZoneHome.vue
+++ training-intern/src/components/exam2/DropZoneHome.vue
@@ -1,9 +1,35 @@
 <template>
-  <div></div>
+  <DropZoneItem @handleSubmit="handleSubmit" :files="getFiles" />
 </template>
 
 <script>
-export default {};
+import DropZoneItem from "./components/DropZoneItem.vue";
+import { storage } from "@/main.js";
+import { ref, uploadBytes } from "firebase/storage";
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  components: {
+    DropZoneItem,
+  },
+  methods: {
+    ...mapActions("file", ["formatFile"]),
+    async handleSubmit(fileRaws) {
+      try {
+        for (let i = 0; i < fileRaws.length; i++) {
+          const fileRef = ref(storage, `files/${fileRaws[i].name}`);
+          await uploadBytes(fileRef, fileRaws[i]);
+        }
+        this.formatFile();
+      } catch (error) {
+        throw Error(error);
+      }
+    },
+  },
+  computed: {
+    ...mapGetters("file", ["getFiles"]),
+  },
+};
 </script>
 
 <style>
